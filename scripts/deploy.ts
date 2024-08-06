4f9bd8e7fabf135f9f596fa48d7e221c1cf2637a
Waseem <wasim_ahmad2003@yahoo.com>
2024-08-06 19:05:13 +0500

    added code to interact with contract

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -10,9 +10,19 @@ async function main() {
   console.log(`Deployed contract to ${address}`)
 
   if (network.config.chainId === 11155111 && process.env.ETHERSCAN_API_KEY) {
+    console.log("Waiting for block txes...")
     await simpleStorage.deploymentTransaction()?.wait(6)
     await verify(address, [])
   }
+
+  const currentValue = await simpleStorage.retrieve()
+  console.log(`Current value is ${currentValue}`)
+
+  // Update the current value
+  const transactionResponse = await simpleStorage.store(7)
+  await transactionResponse.wait(1)
+  const updatedValue = await simpleStorage.retrieve()
+  console.log(`Updated value is: ${updatedValue}`)
 }
 
 async function verify(contractAddress: string, args: any[]) {
