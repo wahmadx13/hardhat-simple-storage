ca3bd448a67e6c894c67437142ceaa32115e4100
Waseem <wasim_ahmad2003@yahoo.com>
2024-08-06 18:57:16 +0500

    added function for verifying transaction

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,11 @@
 import { HardhatUserConfig } from "hardhat/config"
 import "@nomicfoundation/hardhat-toolbox"
+import "@nomicfoundation/hardhat-verify"
 require("dotenv").config()
 
 const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
 const PRIVATE_KEY = process.env.PRIVATE_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 
 const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
@@ -13,6 +15,9 @@ const config: HardhatUserConfig = {
       accounts: [PRIVATE_KEY!],
       chainId: 11155111,
     },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
   },
   solidity: "0.8.8",
 }
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,4 +1,4 @@
-import { ethers } from "hardhat"
+import { ethers, run, network } from "hardhat"
 
 async function main() {
   const SimpleStorageFactory = await ethers.getContractFactory("SimpleStorage")
@@ -8,6 +8,27 @@ async function main() {
   await simpleStorage.waitForDeployment()
   const address = await simpleStorage.getAddress()
   console.log(`Deployed contract to ${address}`)
+
+  if (network.config.chainId === 11155111 && process.env.ETHERSCAN_API_KEY) {
+    await simpleStorage.deploymentTransaction()?.wait(6)
+    await verify(address, [])
+  }
+}
+
+async function verify(contractAddress: string, args: any[]) {
+  console.log("Verifying contract...")
+  try {
+    await run("verify:verify", {
+      address: contractAddress,
+      constructorArguments: args,
+    })
+  } catch (err: any) {
+    if (err.message.toLowerCase().includes("already verified")) {
+      console.log("Already Verified")
+    } else {
+      console.log("The following error occurred while verifying: ", err)
+    }
+  }
 }
 
 main()
