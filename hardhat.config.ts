63851fd0f5788152614cd6a6214a34ced4ce396c
Waseem <wasim_ahmad2003@yahoo.com>
2024-08-07 15:30:41 +0500

    added hardhat gas repoeter configuration

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,10 +3,12 @@ import "@nomicfoundation/hardhat-toolbox"
 import "@nomicfoundation/hardhat-verify"
 require("dotenv").config()
 import "./tasks/block-number"
+import "hardhat-gas-reporter"
 
-const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
-const PRIVATE_KEY = process.env.PRIVATE_KEY
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "https://eth-sepolia"
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0xkey"
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "key"
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "key"
 
 const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
@@ -23,6 +25,14 @@ const config: HardhatUserConfig = {
   },
   etherscan: {
     apiKey: ETHERSCAN_API_KEY,
+  },
+  gasReporter: {
+    enabled: true,
+    outputFile: "gas-report.txt",
+    noColors: true,
+    currency: "USD",
+    // coinmarketcap: COINMARKETCAP_API_KEY,
+    token: "MATIC",
   },
   solidity: "0.8.8",
 }
